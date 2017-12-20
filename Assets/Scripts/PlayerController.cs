e2720bb916d644dcfdffffb4f389a9b2ce56b51d
PC\QUANG <phamkhacquang95@gmail.com>
2017-12-20 22:21:50 +0700

    Thêm comment cho Trang

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ public class PlayerController : MonoBehaviour {
     private void Update() {
         if (GameManager.isPlaying) {
             if (GameManager.isStarted) {
-                if (Input.GetMouseButtonDown(0)) {
+                if (Input.GetMouseButtonDown(0)) {//Sự kiện nhấn chuột hoặc tap vào màn hình
                     Player.State.Jump();
                 }
             }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -12,41 +12,41 @@ public class UIManager : MonoBehaviour {
     public float score;
     private void Start() {
         if (GameManager.isStarted) {
-            tapButtonText.text = "Tap to restart";
+            tapButtonText.text = "Tap to restart";//Đổi chữ start thành restart
             tapButton.gameObject.SetActive(false);
         }
-        highScore = PlayerPrefs.GetInt(highScoreKey, 0);
+        highScore = PlayerPrefs.GetInt(highScoreKey, 0);//Đọc điểm từ bộ nhớ
     }
     private void Update() {
         if (GameManager.isPlaying) {
             string scoreTextTmp = "";
-            if (highScore > 0) scoreTextTmp = "HI: " + highScore.ToString("000000");
-            scoreTextTmp += "  " + Mathf.RoundToInt(score).ToString("000000");
+            if (highScore > 0) scoreTextTmp = "HI: " + highScore.ToString("000000");//Hiện điểm cao nhất nếu có
+            scoreTextTmp += "  " + Mathf.RoundToInt(score).ToString("000000");//Hiện điểm hiện tại
             scoreText.text = scoreTextTmp;
         } else {
             if (GameManager.isStarted) {
-                tapButton.gameObject.SetActive(true);
+                tapButton.gameObject.SetActive(true);//Ẩn chữ tap to start đi
             }
         }
     }
     private void FixedUpdate() {
         if (GameManager.isStarted && GameManager.isPlaying) {
-            score += Time.fixedDeltaTime * 30;
+            score += Time.fixedDeltaTime * 30;//Tăng điểm theo thời gian
         }
     }
 
-    public void TapButtonClick() {
+    public void TapButtonClick() {//Khi chữ Tap to start được nhấn vào
         if (GameManager.isStarted) {
             GameManager.isStarted = false;
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);            
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);//Load lại từ đầu khi chết            
         } else {
             GameManager.isStarted = true;
-            tapButton.gameObject.SetActive(false);
+            tapButton.gameObject.SetActive(false);//Ẩn chữ tap to start đi
         }
     }
 
     private void OnDisable() {
-        if (score > highScore) {
+        if (score > highScore) {//Lưu điểm cao
             PlayerPrefs.SetInt(highScoreKey, (int)score);
             PlayerPrefs.Save();
         }
